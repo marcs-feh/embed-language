e8396c1c08f56abbdbd9a89d405845d3cdf2ecd7
marcos <>
2025-03-06 21:36:10 +0000

    Begin memory interface
    FossilOrigin-Name: b0685c69177b08d593874fa59f0b692d9ca73677d4647149aecbb8fb521d244c

--- base/memory.hpp
+++ base/memory.hpp
@@ -1,6 +1,13 @@
 #pragma once
 #include "base.hpp"
 
+constexpr i64 KiB = 1024ll;
+constexpr i64 MiB = 1024ll * KiB;
+constexpr i64 GiB = 1024ll * MiB;
+constexpr i64 TiB = 1024ll * GiB;
+constexpr i64 PiB = 1024ll * TiB;
+constexpr i64 EiB = 1024ll * PiB;
+
 void* mem_set(void* p, u8 val, isize count);
 
 void* mem_copy(void* dest, void const* source, isize count);
@@ -9,5 +16,36 @@ void* mem_copy_no_overlap(void* dest, void const* source, isize count);
 
 i32 mem_compare(void* pa, void* pb, isize count);
 
-struct Allocator {};
+enum struct AllocatorMode : u8 {
+	Query = 0,
+	Alloc = 1,
+	Resize = 2,
+	Free = 3,
+	FreeAll = 4,
+	AllocNonZero = 5,
+	ResizeNonZero = 6,
+};
+
+// Bit set with the supported AllocatorMode's
+enum struct AllocatorCapabilities : u8 {
+	Query         = 1 << i32(AllocatorMode::Query),
+	Alloc         = 1 << i32(AllocatorMode::Alloc),
+	Resize        = 1 << i32(AllocatorMode::Resize),
+	Free          = 1 << i32(AllocatorMode::Free),
+	FreeAll       = 1 << i32(AllocatorMode::FreeAll),
+	AllocNonZero  = 1 << i32(AllocatorMode::AllocNonZero),
+	ResizeNonZero = 1 << i32(AllocatorMode::ResizeNonZero),
+};
+
+enum struct AllocatorError : u8 {
+	None = 0,
+	OutOfMemory,
+};
+
+using AllocatorFunc = Result<void*, AllocatorError>();
+
+struct Allocator {
+	void* data;
+
+};
 
